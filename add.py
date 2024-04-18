00bf378cd14b8c670d8726074baf013dfd8a311f
Karobi Sarkar <121127329+karobisarkar@users.noreply.github.com>
2024-04-18 16:32:37 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,8 @@
+def add(num1, num2):
+	return num1 + num2
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+
+	print(number_1, "+", number_2, "=",
+					add(number_1, number_2))
